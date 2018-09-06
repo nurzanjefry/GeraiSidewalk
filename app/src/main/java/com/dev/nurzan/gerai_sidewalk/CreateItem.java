e688d9b5d3a0ff1e450f47de9355f28f53c1f01b
nurzanjefry <nurzan.dev@gmail.com>
2018-09-06 23:38:36 +0800

    change layout and include numeric TextEditInput on create_item activity declare all TextEdit, button and add setOnClickListener for button saveItemButton

--- app/src/main/java/com/dev/nurzan/gerai_sidewalk/CreateItem.java
+++ app/src/main/java/com/dev/nurzan/gerai_sidewalk/CreateItem.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -17,11 +18,31 @@ import android.widget.EditText;
 public class CreateItem extends AppCompatActivity {
 
     private static final String TAG = "CreateItem";
-
+    EditText itemName, itemDesc, itemRemark, itemPrice;
+    Button saveItemButton, clearItemButton;
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         Log.d(TAG, "onCreate: CreateItem started");
         setContentView(R.layout.create_item);
+
+        itemName = findViewById(R.id.itemName);
+        itemDesc = findViewById(R.id.itemDesc);
+        itemRemark = findViewById(R.id.itemRemark);
+        itemPrice = findViewById(R.id.itemPrice);
+        saveItemButton = findViewById(R.id.saveItemButton);
+        clearItemButton = findViewById(R.id.clearItemButton);
+
+        saveItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 6/9/2018 save to database 
+                Log.d(TAG, "\nsaveItemButton clicked: " +
+                        "\nitemName: " + itemName.getText().toString()
+                        + "\nitemDesc: " + itemDesc.getText().toString()
+                        + "\nitemRemark: " + itemRemark.getText().toString()
+                        + "\nitemPrice: " + itemPrice.getText().toString());
+            }
+        });
     }
 }
