b06e738d38a14022d1f510fa2418f4b82e2fcf8c
nurzanjefry <nurzan.dev@gmail.com>
2018-09-06 12:11:53 +0800

    added logt & logd on each of activity added openCretateItem to start intent

--- app/src/main/java/com/dev/nurzan/gerai_sidewalk/CreateItem.java
+++ app/src/main/java/com/dev/nurzan/gerai_sidewalk/CreateItem.java
@@ -1,4 +1,26 @@
 package com.dev.nurzan.gerai_sidewalk;
 
-public class CreateItem {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Nurzan Mohd Jefry on 2018096
+ */
+
+
+public class CreateItem extends AppCompatActivity {
+
+    private static final String TAG = "CreateItem";
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        Log.d(TAG, "onCreate: CreateItem");
+        setContentView(R.layout.create_item);
+    }
 }
--- app/src/main/java/com/dev/nurzan/gerai_sidewalk/MainActivity.java
+++ app/src/main/java/com/dev/nurzan/gerai_sidewalk/MainActivity.java
@@ -4,17 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
+
+/**
+ * Created by Nurzan Mohd Jefry on 2018096
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: MainActivity");
         setContentView(R.layout.activity_main);
-
 
 
 
@@ -26,4 +32,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, SalesActivity.class);
         startActivity(intent);
     }
+
+    public void openCreateItem(View view){
+        Intent intent = new Intent(MainActivity.this, CreateItem.class);
+        startActivity(intent);
+    }
+
+
 }
